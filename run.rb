638dd853127e5a15f378c617122adf872d9dfa53
Barbara Brina <bmbrina9@gmail.com>
2017-11-20 21:25:35 -0600

    changes ruby.rb

--- run.rb
+++ run.rb
@@ -1,11 +1,9 @@
 require_relative 'HephaestusLexer'
 require_relative 'HephaestusParser'
 
-#input = ANTLR3::FileStream.new('Tests/matrixMultiplication.txt')
-#input = ANTLR3::FileStream.new('Tests/recursiveFibonacci.txt')
-#input = ANTLR3::FileStream.new('Tests/bubbleSort.txt')
-#input = ANTLR3::FileStream.new('Tests/recursiveFactorial.txt')
-input = ANTLR3::FileStream.new('Tests/object_test.txt')
+file_name = "Tests/object_test.txt"
+
+input = ANTLR3::FileStream.new(file_name)
 $lexer = Hephaestus::Lexer.new( input )
 
 def printTokens()
@@ -18,9 +16,5 @@ end
 
 #printTokens()
 
-#parser = open('Tests/matrixMultiplication.txt') { | f | Hephaestus::Parser.new( f ) }
-#parser = open('Tests/recursiveFibonacci.txt') { | f | Hephaestus::Parser.new( f ) }
-#parser = open('Tests/bubbleSort.txt') { | f | Hephaestus::Parser.new( f ) }
-#parser = open('Tests/recursiveFactorial.txt') { | f | Hephaestus::Parser.new( f ) }
-parser = open('Tests/object_test.txt') { | f | Hephaestus::Parser.new( f ) }
+parser = open(file_name) { | f | Hephaestus::Parser.new( f ) }
 parser.start
