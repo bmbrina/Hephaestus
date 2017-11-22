963dfac4e9e31297016cf1eb541e3824739f88cf
Barbara Brina <bmbrina9@gmail.com>
2017-11-22 09:29:55 -0600

    Modifies bash scripts

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ if [ -z $hephaestus ]; then
     echo 'export PATH="$HOME/.hephaestus:$PATH"' >> ~/.zshrc
 fi
 
+eval chmod +x hephaestus
 echo 'Hephaestus is installed!'
 echo 'Restart your terminal to start and happy coding!'
